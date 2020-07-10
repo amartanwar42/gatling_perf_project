8f211475c852d2ca841e96b836a17e8e02596cdf
Amar Singh Tanwar <a.tanwar@geton-institut.de>
2020-07-10 09:55:58 +0200

    Customizing sbt project

--- src/test/scala/computerdatabase/ComputerWorld.scala
+++ src/test/scala/computerdatabase/ComputerWorld.scala
@@ -1,43 +1,41 @@
 package computerdatabase
 
 import scala.concurrent.duration._
-
 import java.util.concurrent.ThreadLocalRandom
 
 import io.gatling.core.Predef._
+import io.gatling.core.structure.ChainBuilder
 import io.gatling.http.Predef._
 
 class ComputerWorld extends Simulation {
-
   val httpProtocol = http
-    .baseUrl("http://computer-database.gatling.io")
-    .acceptHeader("""text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8""")
-    .acceptEncodingHeader("""gzip, deflate""")
-    .acceptLanguageHeader("""en-gb,en;q=0.5""")
-    .userAgentHeader("""Mozilla/5.0 (Macintosh; Intel Mac OS X 10.9; rv:31.0) Gecko/20100101 Firefox/31.0""")
+      .baseUrl("https://apis-staging.hellobetter.de")
 
-  val computerDbScn = scenario("Computer Scenario") 
-    .exec(http("getComputers")
-    .get("/computers")
-      .check(
-        status is 200,
-        regex("""\d+ computers found"""),
-        css("#add", "href").saveAs("addComputer")))
+  val headers_0 = Map(
+    "Content-Type" -> "application/json")
 
-    .exec(http("addNewComputer")
-    .get("${addComputer}")
-      .check(substring("Add a computer")))
+  object login{
+    val feeder = csv("sample.csv").circular
+    val login = feed(feeder)
+      .exec(http("Service")
+        .post("/api/v1/login")
+        .headers(headers_0)
+        .body(StringBody(
+        """{"email":"${eml}","password":"${pwd}"}""")
+        )
+        .check(bodyString.saveAs("BODY"))
+        .check(status is 200)
+      ).exec(session => {
+      val response = session("BODY").as[String]
+      println(response)
+      session
+    })
+  }
 
-    .exec(_.set("homeComputer", s"homeComputer_${ThreadLocalRandom.current.nextInt(Int.MaxValue)}"))
-    .exec(http("postComputers") 
-    .post("/computers")
-      .formParam("name", "${homeComputer}") 
-      .formParam("introduced", "2015-10-10") 
-      .formParam("discontinued", "2017-10-10") 
-      .formParam("company", "") 
-      .check(substring("${homeComputer}")))
+  val scn=scenario("login")
+    .exec(login.login)
 
-  setUp(computerDbScn.inject(
-    constantUsersPerSec(2) during(1 minute)
-  ).protocols(httpProtocol)) 
+  setUp(scn.inject(
+    constantUsersPerSec(1) during(1 second)
+  )).protocols(httpProtocol)
 }
